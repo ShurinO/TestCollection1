3a7fb9f41358a2d006f58a7aa6cc192808337b82
Oleg <arestarx@ukr.net>
2018-07-08 14:08:24 +0300

    Initial commit

---
+++ src/com/brainacad/oop/testcollection1/Main.java
@@ -0,0 +1,16 @@
+package com.brainacad.oop.testcollection1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      list.add("number_"+i);
+    }
+    for (String s : list) {
+      System.out.println(s);
+    }
+  }
+}
